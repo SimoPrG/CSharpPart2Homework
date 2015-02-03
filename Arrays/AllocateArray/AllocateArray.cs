56a3da829afc26238797ec8163f939815fd590b2
SimoPrG <SimoPrG@users.noreply.github.com>
2015-02-04 00:06:12 +0200

    Solution to Problem 1. Allocate array

---
+++ Arrays/AllocateArray/AllocateArray.cs
@@ -0,0 +1,28 @@
+﻿/*Problem 1. Allocate array
+
+    Write a program that allocates array of 20 integers and initializes each element by its index multiplied by 5.
+    Print the obtained array on the console.
+*/
+
+using System;
+
+class AllocateArray
+{
+    static void Main()
+    {
+        Console.Title = "Allocate Array";
+
+        int[] myFirstArray = new int[20];
+        for (int i = 0; i < myFirstArray.Length; i++)
+        {
+            myFirstArray[i] = i * 5;
+        }
+
+        for (int i = 0; i < myFirstArray.Length; i++)
+        {
+            Console.WriteLine("myFirstArray[{0,2}] = {1,2}", i, myFirstArray[i]);
+        }
+
+        Console.ReadKey();
+    }
+}
