ba3ed2df43089ade2b72bff7c682056202934e1a
SimoPrG <SimoPrG@users.noreply.github.com>
2015-02-23 21:47:14 +0200

    Problem 6. Sum integers

---
+++ UsingClassesAndObjects/SumIntegers/SumIntegers.cs
@@ -0,0 +1,26 @@
+﻿/*Problem 6. Sum integers
+
+    You are given a sequence of positive integer values written into a string, separated by spaces.
+    Write a function that reads these values from given string and calculates their sum.
+
+Example:
+input 	output
+"43 68 9 23 318" 	461
+*/
+
+using System;
+using System.Linq;
+
+class SumIntegers
+{
+    static void Main()
+    {
+        Console.Title = "Sum Integers";
+
+        Console.WriteLine("Please, enter some integers separated by space!");
+        int[] array = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+        Console.WriteLine("The sum is {0}", array.Sum());
+
+        Console.ReadKey();
+    }
+}
