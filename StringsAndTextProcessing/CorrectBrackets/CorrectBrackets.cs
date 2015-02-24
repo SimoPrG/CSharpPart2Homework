ffc061ef66bc2351b521d4fae2db563a70932522
SimoPrG <SimoPrG@users.noreply.github.com>
2015-02-24 17:33:02 +0200

    Problems 1, 2, 3

---
+++ StringsAndTextProcessing/CorrectBrackets/CorrectBrackets.cs
@@ -0,0 +1,32 @@
+﻿/*Problem 3. Correct brackets
+
+    Write a program to check if in a given expression the brackets are put correctly.
+
+Example of correct expression: ((a+b)/5-d). Example of incorrect expression: )(a+b)).
+*/
+
+using System;
+using System.Linq;
+
+class CorrectBrackets
+{
+    static void Main()
+    {
+        string expression = "((a+b)/5-d)";
+        Console.WriteLine("{0} brackets {1} corect.",
+            expression, AreBracketsCorrect(expression) ? "are" : "are NOT");
+
+        expression = ")(a+b))";
+        Console.WriteLine("{0} brackets {1} corect.",
+            expression, AreBracketsCorrect(expression) ? "are" : "are NOT");
+
+        Console.ReadKey();
+    }
+
+    static bool AreBracketsCorrect(string expression)
+    {
+        if (expression.Count(c => c == '(') == expression.Count(c => c == ')'))
+            return true;
+        return false;
+    }
+}
---
+++ StringsAndTextProcessing/ReverseString/StringReversing.cs
@@ -0,0 +1,31 @@
+﻿/*Problem 2. Reverse string
+
+    Write a program that reads a string, reverses it and prints the result at the console.
+
+Example:
+input 	output
+sample 	elpmas
+*/
+
+using System;
+
+class StringReversing
+{
+    static void Main()
+    {
+        string alpha = "ABCDEFGHIJKLMNOPQRSTUVQXYZ";
+        Console.WriteLine(alpha);
+
+        string reverseAlpha = ReverseString(alpha);
+        Console.WriteLine("Reversed:\n{0}", reverseAlpha);
+
+        Console.ReadKey();
+    }
+
+    static string ReverseString (string str)
+    {
+        char[] array = str.ToCharArray();
+        Array.Reverse(array);
+        return new string(array);
+   }
+}
