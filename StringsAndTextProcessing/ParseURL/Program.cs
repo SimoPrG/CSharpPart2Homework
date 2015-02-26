fe37294c7e1cb15775d263f630b24074e411b2e2
SimoPrG <SimoPrG@users.noreply.github.com>
2015-02-27 00:59:24 +0200

    Problem 12. Parse URL

---
+++ StringsAndTextProcessing/ParseURL/Program.cs
@@ -0,0 +1,30 @@
+﻿/*Problem 12. Parse URL
+
+    Write a program that parses an URL address given in the format: [protocol]://[server]/[resource] and extracts
+    from it the [protocol], [server] and [resource] elements.
+
+Example:
+URL 	                                                Information
+http://telerikacademy.com/Courses/Courses/Details/212 	[protocol] = http
+                                                        [server] = telerikacademy.com
+                                                        [resource] = /Courses/Courses/Details/212
+*/
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Please, enter an URL: ");
+        string url = Console.ReadLine();
+
+        Uri uri = new Uri(url);
+
+        Console.WriteLine("[protocol] = {0}", uri.Scheme);
+        Console.WriteLine("[server] = {0}", uri.Host);
+        Console.WriteLine("[resource] = {0}", uri.AbsolutePath);
+
+        Console.ReadKey();
+    }
+}
