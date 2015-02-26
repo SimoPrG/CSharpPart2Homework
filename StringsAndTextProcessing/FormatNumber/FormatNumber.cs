62444660a6f3e3679d2bb90b819bc4c77ba04bb2
SimoPrG <SimoPrG@users.noreply.github.com>
2015-02-27 00:41:09 +0200

    Problem 11. Format number

---
+++ StringsAndTextProcessing/FormatNumber/FormatNumber.cs
@@ -0,0 +1,24 @@
+﻿/*Problem 11. Format number
+
+    Write a program that reads a number and prints it as a decimal number, hexadecimal number, percentage and in scientific notation.
+    Format the output aligned right in 15 symbols.
+*/
+
+using System;
+
+class FormatNumber
+{
+    static void Main()
+    {
+        int number;
+
+        do
+        {
+            Console.Write("Please, enter an integer: ");
+        } while (!int.TryParse(Console.ReadLine(), out number));
+
+        Console.WriteLine("{0, 15:D}\n{0, 15:X}\n{0, 15:P}\n{0, 15:E}\n", number);
+
+        Console.ReadKey();
+    }
+}
