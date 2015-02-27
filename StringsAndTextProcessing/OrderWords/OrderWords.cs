ece1a56872a6ef0f2195d7ff66fe6b24cd906032
SimoPrG <SimoPrG@users.noreply.github.com>
2015-02-27 11:54:41 +0200

    Problem 24. Order words

---
+++ StringsAndTextProcessing/OrderWords/OrderWords.cs
@@ -0,0 +1,19 @@
+﻿/*Problem 24. Order words
+
+    Write a program that reads a list of words, separated by spaces and prints the list in an alphabetical order.
+*/
+
+using System;
+
+class OrderWords
+{
+    static void Main()
+    {
+        Console.WriteLine("Please, enter some words separated by spaces!");
+        string input = Console.ReadLine();
+        string[] sortedWords = input.Split();
+        Array.Sort(sortedWords);
+        Console.WriteLine(string.Join("\n", sortedWords));
+        Console.ReadKey();
+    }
+}
