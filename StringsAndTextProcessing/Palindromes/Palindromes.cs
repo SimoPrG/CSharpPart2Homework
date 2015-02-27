ecaa95087da30d987d41261808d72f832d212acd
SimoPrG <SimoPrG@users.noreply.github.com>
2015-02-27 08:08:58 +0200

    Problem 20. Palindromes

---
+++ StringsAndTextProcessing/Palindromes/Palindromes.cs
@@ -0,0 +1,34 @@
+﻿/*Problem 20. Palindromes
+
+    Write a program that extracts from a given text all palindromes, e.g. ABBA, lamal, exe.
+*/
+
+using System;
+using System.Text.RegularExpressions;
+
+class Palindromes
+{
+    static void Main()
+    {
+        string text = "C# is not C++, not PHP and not Delphi! Write a program that extracts from a given text all palindromes, e.g. ABBA, lamal, exe.";
+
+        string regex = @"[,;:\-\!\?\.\s]+";
+
+        foreach (var word in Regex.Split(text, regex))
+        {
+            if (word.Length > 1 && word.ToLower().Equals(ReverseString(word.ToLower())))
+            {
+                Console.WriteLine(word);
+            }
+        }
+
+        Console.ReadKey();
+    }
+
+    static string ReverseString(string str)
+    {
+        char[] array = str.ToCharArray();
+        Array.Reverse(array);
+        return new string(array);
+    }
+}
