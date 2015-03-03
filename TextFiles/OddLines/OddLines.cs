d7cb408765c4c321ab59f0b2ca027563d3aef7d5
SimoPrG <SimoPrG@users.noreply.github.com>
2015-03-03 11:52:27 +0200

    Problem 1. Odd lines

---
+++ TextFiles/OddLines/OddLines.cs
@@ -0,0 +1,25 @@
+﻿/*Problem 1. Odd lines
+
+    Write a program that reads a text file and prints on the console its odd lines.
+*/
+
+using System;
+using System.IO;
+class OddLines
+{
+    static void Main()
+    {
+        using(StreamReader reader = new StreamReader(@"..\..\TextFile.txt"))
+        {
+            string line = reader.ReadLine();
+            while (line != null)
+            {
+                Console.WriteLine(line);
+                reader.ReadLine();
+                line = reader.ReadLine();               
+            }
+        }
+
+        Console.ReadKey();
+    }
+}
