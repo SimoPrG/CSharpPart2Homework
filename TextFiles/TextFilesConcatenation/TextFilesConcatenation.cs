1abaa9cd9bbef46452e0aa2e61f1b972b6f156bd
SimoPrG <SimoPrG@users.noreply.github.com>
2015-03-03 12:27:55 +0200

    Problem 2. Concatenate text files

---
+++ TextFiles/TextFilesConcatenation/TextFilesConcatenation.cs
@@ -0,0 +1,32 @@
+﻿/*Problem 2. Concatenate text files
+
+    Write a program that concatenates two text files into another text file.
+*/
+
+using System;
+using System.IO;
+class TextFilesConcatenation
+{
+    static void Main()
+    {
+        using (StreamReader reader = new StreamReader(@"..\..\TextFile1.txt"))
+        {
+            string buffer = reader.ReadToEnd();
+
+            using (StreamWriter writer = new StreamWriter(@"..\..\Result.txt"))
+            {
+                writer.Write(buffer);
+            }
+        }
+
+        using (StreamReader reader = new StreamReader(@"..\..\TextFile2.txt"))
+        {
+            string buffer = reader.ReadToEnd();
+
+            using (StreamWriter writer = new StreamWriter(@"..\..\Result.txt", true))
+            {
+                writer.Write(buffer);
+            }
+        }
+    }
+}
