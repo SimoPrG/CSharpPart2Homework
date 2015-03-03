82c5dd80887cf36a282dfeaa3ddbb749fd67a17d
SimoPrG <SimoPrG@users.noreply.github.com>
2015-03-03 22:16:05 +0200

    Problem 11. Prefix "test"

---
+++ TextFiles/PrefixTest/PrefixTest.cs
@@ -0,0 +1,49 @@
+﻿/*Problem 11. Prefix "test"
+
+    Write a program that deletes from a text file all words that start with the prefix test.
+    Words contain only the symbols 0…9, a…z, A…Z, _.
+*/
+namespace PrefixTest
+{
+    using System;
+    using System.IO;
+    using System.Text;
+    using System.Linq;
+    class PrefixTest
+    {
+        static void Main(string[] args)
+        {
+            string inputPath = @"..\..\input.txt";
+            string outputPath = @"..\..\output.txt";
+            StringBuilder result = new StringBuilder();
+
+            try
+            {
+                using (StreamReader reader = new StreamReader(inputPath))
+                {
+                    string currentLine;
+                    while (!reader.EndOfStream)
+                    {
+                        currentLine = reader.ReadLine();
+                        string[] separatedWords = currentLine
+                            .Split(new char[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries)
+                            .Where(x => !x.StartsWith("test", StringComparison.OrdinalIgnoreCase))
+                            .ToArray();
+
+                        result.AppendLine(String.Join(" ", separatedWords));
+                    }
+                }
+
+
+                using (StreamWriter writer = new StreamWriter(outputPath))
+                {
+                    writer.Write(result.ToString());
+                }
+            }
+            catch (IOException exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+        }
+    }
+}
