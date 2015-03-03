54f44276f9a4fe5512ef90a0df626cd1ea9c4d7c
SimoPrG <SimoPrG@users.noreply.github.com>
2015-03-03 19:38:20 +0200

    Problem 8. Replace whole word

---
+++ TextFiles/WholeWordReplacement/WholeWordReplacement.cs
@@ -0,0 +1,37 @@
+﻿/*Problem 8. Replace whole word
+
+    Modify the solution of the previous problem to replace only whole words (not strings).
+*/
+
+using System;
+using System.IO;
+using System.Text.RegularExpressions;
+class SubStringReplacement
+{
+    static void Main()
+    {
+        try
+        {
+            using (StreamReader reader = new StreamReader(@"..\..\big.txt"))
+            {
+                using (StreamWriter writer = new StreamWriter(@"..\..\temp.txt"))
+                {
+                    string line;
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        writer.WriteLine(Regex.Replace(line, @"\bstart\b", m => m.Value[0] == 'S' ? "Finish" : "finish",
+                            RegexOptions.IgnoreCase));
+                    }
+                }
+            }
+
+            File.Delete(@"..\..\big.txt");
+            File.Move(@"..\..\temp.txt", @"..\..\big.txt");
+        }
+        catch (IOException exc)
+        {
+            Console.WriteLine("Error: {0}.", exc.Message);
+        }
+    }
+
+}
