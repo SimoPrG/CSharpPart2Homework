62a2fd2e1b1777094fc6e3132da58c7bf394a1ce
SimoPrG <SimoPrG@users.noreply.github.com>
2015-03-03 12:56:40 +0200

    Problem 3. Line numbers Problem 4. Compare text files

---
+++ TextFiles/LineNumbers/LineNumbers.cs
@@ -0,0 +1,29 @@
+﻿/*Problem 3. Line numbers
+
+    Write a program that reads a text file and inserts line numbers in front of each of its lines.
+    The result should be written to another text file.
+*/
+
+using System;
+using System.IO;
+class LineNumbers
+{
+    static void Main()
+    {
+        using (StreamReader reader = new StreamReader(@"..\..\TextFile.txt"))
+        {
+            string line = reader.ReadLine();
+            int count = 1;
+
+            using (StreamWriter writer = new StreamWriter(@"..\..\Result.txt"))
+            {
+                while (line != null)
+                {
+                    writer.WriteLine("{0}. {1}", count, line);
+                    count++;
+                    line = reader.ReadLine();
+                }
+            }
+        }
+    }
+}
---
+++ TextFiles/TextFilesComparison/TextFilesComparison.cs
@@ -0,0 +1,46 @@
+﻿/*Problem 4. Compare text files
+
+    Write a program that compares two text files line by line and prints the number of lines that are the same
+    and the number of lines that are different.
+    Assume the files have equal number of lines.
+*/
+
+using System;
+using System.IO;
+class TextFilesComparison
+{
+    static void Main()
+    {
+        int sameLines = 0;
+        int differentLines = 0;
+
+        using (StreamReader reader1 = new StreamReader(@"..\..\TextFile1.txt"))
+        {
+            using (StreamReader reader2 = new StreamReader(@"..\..\TextFile2.txt"))
+            {
+                string line1 = reader1.ReadLine();
+                string line2 = reader2.ReadLine();
+
+                while (line1 != null && line2 != null)
+                {
+                    if (line1 == line2)
+                    {
+                        sameLines++;
+                    }
+                    else
+                    {
+                        differentLines++;
+                    }
+
+                    line1 = reader1.ReadLine();
+                    line2 = reader2.ReadLine();
+                }
+            }
+        }
+
+        Console.WriteLine("Same lines: {0}", sameLines);
+        Console.WriteLine("Different lines: {0}", differentLines);
+
+        Console.ReadLine();
+    }
+}
