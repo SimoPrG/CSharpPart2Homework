7943e23af3c43d4969693a7c861f47aa6fc05e86
SimoPrG <SimoPrG@users.noreply.github.com>
2015-03-03 18:52:07 +0200

    Problem 7. Replace sub-string

---
+++ TextFiles/SubStringReplacement/SubStringReplacement.cs
@@ -0,0 +1,35 @@
+﻿/*Problem 7. Replace sub-string
+
+    Write a program that replaces all occurrences of the sub-string start with the sub-string finish in a text file.
+    Ensure it will work with large files (e.g. 100 MB).
+*/
+
+using System;
+using System.IO;
+class SubStringReplacement
+{
+    static void Main()
+    {
+        try
+        {
+            using (StreamReader reader = new StreamReader(@"..\..\big.txt"))
+            {
+                using (StreamWriter writer = new StreamWriter(@"..\..\temp.txt"))
+                {
+                    string line;
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        writer.WriteLine(line.Replace("start", "finish"));
+                    }
+                }
+            }
+
+            File.Delete(@"..\..\big.txt");
+            File.Move(@"..\..\temp.txt", @"..\..\big.txt");
+        }
+        catch (IOException exc)
+        {
+            Console.WriteLine("Error: {0}.", exc.Message);
+        }
+    }
+}
